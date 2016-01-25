07de6afd3ffa81fe7d17066ac7a089e0de6ea1b0
mattias0h <hintmattias@gmail.com>
2016-01-25 12:05:57 +0200

    Lecture 15 - Learn how to create the parallax scrolling effect in less than 5 minutes [#111841409]

--- B2W/assets/js/main.js
+++ B2W/assets/js/main.js
@@ -1,3 +1,16 @@
 /**
  * Created by Mattias on 19.01.2016.
  */
+
+$(document).ready(function () {
+    var $window = $(window);
+
+    $('section[data-type="background"]').each(function () {
+        var $bgobj = $(this);
+        $(window).scroll(function () {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            var coords = '50% ' + yPos + 'px';
+            $bgobj.css({backgroundPosition: coords});
+        });
+    });
+});
