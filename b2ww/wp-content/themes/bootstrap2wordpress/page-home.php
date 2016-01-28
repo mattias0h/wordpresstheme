7b95720ad3d6249ddef17853795fedb0776a3f9c
mattias0h <hintmattias@gmail.com>
2016-01-28 17:51:03 +0200

    Lecture 48 - Code hero & use custom fields [#111841449]

--- b2ww/wp-content/themes/bootstrap2wordpress/page-home.php
+++ b2ww/wp-content/themes/bootstrap2wordpress/page-home.php
@@ -2,6 +2,16 @@
 /*
     Template Name: Home Page
  */
+
+//Custom fields
+$prelaunch_price        =  get_post_meta(5, 'prelaunch_price', true);
+$launch_price           =  get_post_meta(5, 'launch_price', true);
+$final_price            =  get_post_meta(5, 'final_price', true);
+$course_url             =  get_post_meta(5, 'course_url', true);
+$button_text             =  get_post_meta(5, 'button_text', true);
+$optin_text             =  get_post_meta(5, 'optin_text', true);
+$optin_button_text      =  get_post_meta(5, 'optin_button_text', true);
+
 get_header(); ?>
 
     <section id="hero" data-type="background" data-speed="5">
@@ -14,34 +24,34 @@ get_header(); ?>
                     </div>
 
                     <div class="col-sm-7 hero-text">
-                        <h1>BootStrap Wordpress</h1>
+                        <h1><?php bloginfo('name'); ?></h1>
 
-                        <p class="lead">Earn by Learning Bootstrap Wordpress</p>
+                        <p class="lead"><?php bloginfo('description'); ?></p>
 
                         <div id="price-timeline">
                             <div class="price active">
                                 <h4>Pre-launch Price
                                     <small>Buy!</small>
                                 </h4>
-                                <span>149$</span>
+                                <span><?php echo $prelaunch_price; ?></span>
                             </div>
 
                             <div class="price">
                                 <h4>Launch Price
                                     <small>Buy!</small>
                                 </h4>
-                                <span>299$</span>
+                                <span><?php echo $launch_price; ?></span>
                             </div>
 
                             <div class="price">
                                 <h4>Final Price
                                     <small>Buy!</small>
                                 </h4>
-                                <span>399$</span>
+                                <span><?php echo $final_price; ?></span>
                             </div>
 
                         </div>
-                        <p><a class="btn btn-lg btn-danger" href="/" role="button">Enroll &raquo;</a></p>
+                        <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>" role="button"><?php echo $button_text; ?></a></p>
                     </div>
 
                 </div>
@@ -53,14 +63,11 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-sm-8">
-                    <p class="lead"><strong>Subscribe to our mailing list.</strong>We'll send something special as thank
-                        you.</p>
+                    <p class="lead"><?php echo $optin_text; ?></p>
                 </div>
 
                 <div class="col-sm-4">
-                    <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">Click Here
-                        To Subscribe
-                    </button>
+                    <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal"><?php echo $optin_button_text; ?></button>
                 </div>
             </div>
         </div>
