6ddd466c88f8963cfd7b56721e273157140f17be
mattias0h <hintmattias@gmail.com>
2016-01-28 16:13:23 +0200

    Lecture 43 - Convert your static header to a dynamic header [#111841441]

--- b2ww/wp-content/themes/bootstrap2wordpress/header.php
+++ b2ww/wp-content/themes/bootstrap2wordpress/header.php
@@ -24,27 +24,27 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrap2wordpress' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<header class="site-header" role="banner">
+		<div class="navbar-wrapper">
+			<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="glyphicon glyphicon-option-horizontal"></span>
+						</button>
+						<a href="/" class="navbar-brand"><img src="assets/img/logo.png" alt="BootStrap Wordpress"></a>
+					</div>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bootstrap2wordpress' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+					<?php
+					wp_nav_menu( array(
+							'theme-location' => 'primary',
+							'container' => 'nav',
+							'container-class' => 'navbar-collapse collapse',
+							'menu_class' => 'nav navbar-nav navbar-right'
+					));
+					?>
+				</div>
+			</div>
+		</div>
+	</header>
