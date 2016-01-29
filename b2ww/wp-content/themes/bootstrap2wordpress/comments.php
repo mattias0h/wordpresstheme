ac32e5cb29014e33221bd508e8620ac46178a533
mattias0h <hintmattias@gmail.com>
2016-01-29 17:18:32 +0200

    Lecture 64 - Style and customize the comments section [#111841459]

--- b2ww/wp-content/themes/bootstrap2wordpress/comments.php
+++ b2ww/wp-content/themes/bootstrap2wordpress/comments.php
@@ -9,7 +9,6 @@
  *
  * @package Bootstrap_to_Wordpress
  */
-
 /*
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
@@ -22,64 +21,64 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
+	<div class="comments-wrap"></div>
+
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+		<h3 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'bootstrap2wordpress' ) ),
+			printf( // WPCS: XSS OK.
+					esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'bootstrap' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
-				);
+			);
 			?>
-		</h2>
+		</h3>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'bootstrap2wordpress' ); ?></h2>
-			<div class="nav-links">
+			<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'bootstrap' ); ?></h2>
+				<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'bootstrap2wordpress' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'bootstrap2wordpress' ) ); ?></div>
+					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'bootstrap' ) ); ?></div>
+					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'bootstrap' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
+				</div><!-- .nav-links -->
+			</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
+			wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
-				) );
+			) );
 			?>
 		</ol><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'bootstrap2wordpress' ); ?></h2>
-			<div class="nav-links">
+			<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'bootstrap' ); ?></h2>
+				<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'bootstrap2wordpress' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'bootstrap2wordpress' ) ); ?></div>
+					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'bootstrap' ) ); ?></div>
+					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'bootstrap' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php
+				</div><!-- .nav-links -->
+			</nav><!-- #comment-nav-below -->
+			<?php
 		endif; // Check for comment navigation.
-
 	endif; // Check for have_comments().
-
-
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bootstrap2wordpress' ); ?></p>
-	<?php
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bootstrap' ); ?></p>
+		<?php
 	endif;
-
 	comment_form();
 	?>
+
+</div>
 
 </div><!-- #comments -->
