86d88c2f84e89755889cf64aaf3848c86d14335b
mattias0h <hintmattias@gmail.com>
2016-01-29 17:34:54 +0200

    Lecture 66 - Customize the search results template [Delivers #111841459]

--- b2ww/wp-content/themes/bootstrap2wordpress/search.php
+++ b2ww/wp-content/themes/bootstrap2wordpress/search.php
@@ -6,43 +6,42 @@
  *
  * @package Bootstrap_to_Wordpress
  */
-
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title"><?php printf(esc_html__('Search Results for: %s', 'bootstrap'), '<span>' . get_search_query() . '</span>'); ?></h1>
+	</section>
 
-		<?php
-		if ( have_posts() ) : ?>
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
+				<?php
+				if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+					<?php
+					/* Start the Loop */
+					while (have_posts()) : the_post();
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part('template-parts/content', 'search');
+					endwhile;
+					the_posts_navigation();
+				else :
+					get_template_part('template-parts/content', 'none');
+				endif; ?>
+			</main>
+			<!-- content -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+			<!-- SIDEBAR ========================= -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+		</div>
+		<!-- primary -->
+	</div><!-- content -->
 
 <?php
-get_sidebar();
 get_footer();
